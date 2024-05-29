fd36e63ded68ec24f10e4987bebb99c8c3804231
amannegi <akuro787898@gmail.com>
2024-05-30 00:44:17 +0530

    fix: update origin list

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(
   cors({
     origin: [
-      'https://moonlit-salmiakki-eae0cc.netlify.app',
+      'https://devbhoomi-fresh.netlify.app',
       'http://localhost:5173',
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
